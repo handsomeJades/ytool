c81f30efe9fc05f7b282a4d246666a815dfd5e73
shitingyu <shitingyu@100tal.com>
2018-06-13 17:35:32 +0800

    20180613

---
+++ cprimeplus/char_io.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int swap1(int,int);
+int main()
+{
+//	FILE * fp=fopen("tmp.txt","w+");
+//	
+//	printf("%d\n",sizeof(stdin));
+//		
+//	fprintf(fp,"Hello %s!\n","jades");
+	
+	char a[10];
+	scanf("%*s");
+	scanf("%s",a);
+	printf("%s\n",a);
+	printf("%d\n",swap1(1,2));
+}
+
+int swap1(int a,int b){
+	return 6;
+
+}

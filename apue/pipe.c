1a99ffb2c3b82dec2ecd091b503575eea4080e96
shitingyu <shitingyu@100tal.com>
2018-07-01 17:29:57 +0800

    匿名管道

---
+++ apue/pipe.c
@@ -0,0 +1,29 @@
+#include "include/apue.h"
+
+int main()
+{
+	int n;
+	int fd[2];
+	pid_t pid;
+	char line[15];
+	//create semi-pipe
+	if(pipe(fd)<0){
+		perror("pipe");
+	}
+
+	if((pid=fork())<0){
+		perror("创建进程");
+	}else if(pid>0) {
+		puts("this is parent process...");
+//		close(fd[0]);
+		write(fd[1],"hello world\n",12);
+	}else{
+		puts("this is child process...");
+//		close(fd[1]);
+		n=read(fd[0],line,7);
+		puts(line);
+	}
+
+	return 0;
+
+}
